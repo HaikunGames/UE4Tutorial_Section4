2be2c46a1462d956a7713e4dbceb2a2779e71046
haikun.huang <quincy.huanghk@gmail.com>
2018-12-07 20:34:06 -0500

    Initial commit

---
+++ Source/Section4/Section4GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Section4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION4_API ASection4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
